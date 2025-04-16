769fa80c6c7aed2669dfc79afa65e3e75261760a
Guzel Zaripova <guzel.zhagfarovna@gmail.com>
2025-04-16 12:38:09 +0500

    Класс представления Contacts

---
+++ src/components/view/Contacts.ts
@@ -0,0 +1,19 @@
+import { TContacts } from '../../types';
+import { IEvents } from '../base/Events';
+import { Form } from './Form';
+
+export class Contacts extends Form<TContacts> {
+	constructor(container: HTMLFormElement, events: IEvents) {
+		super(container, events);
+	}
+
+	set email(value: string) {
+		(this.container.elements.namedItem('email') as HTMLInputElement).value =
+			value;
+	}
+
+	set phone(value: string) {
+		(this.container.elements.namedItem('phone') as HTMLInputElement).value =
+			value;
+	}
+}
